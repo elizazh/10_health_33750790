8eb1d83cf156f2c3ba474344756e64e9e1d82c7c
elizazh <149685559+elizazh@users.noreply.github.com>
2025-12-12 14:02:21 +0000

    Create db.js

---
+++ db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.HEALTH_HOST || 'localhost',
+  user: process.env.HEALTH_USER || 'health_app',
+  password: process.env.HEALTH_PASSWORD || 'qwertyuiop',
+  database: process.env.HEALTH_DATABASE || 'health',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool;
